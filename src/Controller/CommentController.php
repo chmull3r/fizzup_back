cb91a5e42f17f1e2533b2016ded2ccbbecae62b6
Charlotte Muller <chmullerweb@gmail.com>
2023-04-27 16:24:11 +0200

    setup form page style and layout

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -6,13 +6,14 @@ use App\Form\CommentType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Comment;
 
 class CommentController extends AbstractController
 {
-    #[Route('/comment', name: 'app_comment')]
+    #[Route('/', name: 'app_homepage')]
     public function new(Request $request, EntityManagerInterface $entityManager): JsonResponse
     {
         $file = "https://img.freepik.com/vecteurs-libre/caricature-chien-beagle-fond-blanc_1308-68249.jpg?w=2000";
@@ -26,7 +27,6 @@ class CommentController extends AbstractController
         // check if data respect formType rules
         $form = $this->createForm(CommentType::class, $comment);
 
-        dd($form->isSubmitted());
         if ($form->isSubmitted() && $form->isValid()) {
             dd($form['image']->getData());
         }
@@ -37,8 +37,41 @@ class CommentController extends AbstractController
         // $entityManager->flush();
 
         return new JsonResponse(
-            ['form' => $form]
+           ['form' => $form]
             //['result' => $comment->getId()]
         );
+    }
+
+    #[Route('/comments', name: 'app_comments_list')]
+    public function list(): Response
+    {
+        $article = [
+            'image' => '/images/magic-photo.png',
+            'question' => 'un corgi peut-il marcher 30km dans les Vosges en une seule et même journée ?',
+            'author' => 'moi',
+            'content' => "ouiuouiouiuouiuouiuouiuouiu"
+        ];
+
+        $commentsList = [
+            0 => [
+                'pseudo' => 'troisticha',
+                'email' => 'troisticha@miaou.fr',
+                'note' => '3',
+                'text' => 'lulululululululululul',
+                'image' => "/images/tisha.png",
+            ],
+            1 => [
+                'pseudo' => 'ticha',
+                'email' => 'ticha@miaou.fr',
+                'note' => '5',
+                'text' => 'miaoumiaoumioaumiaou',
+                'image' => "/images/tisha.png",
+            ]
+        ];
+
+        return $this->render('comment/index.html.twig', [
+            'article' => $article,
+            'comments' => $commentsList,
+        ]);
     }
 }
