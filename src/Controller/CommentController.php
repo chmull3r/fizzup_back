dbc4712260d26e9a7afe531ed41a3073b319017a
Charlotte Muller <chmullerweb@gmail.com>
2023-05-04 15:44:44 +0200

    dump data sql init

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Form\CommentType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,67 +14,76 @@ use App\Entity\Comment;
 
 class CommentController extends AbstractController
 {
-    #[Route('/comment/new', name: 'app_add_comment', methods:'POST')]
+    #[Route('/comment/new', name: 'app_add_comment', methods:['POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): JsonResponse
     {
         $comment = new Comment();
         $comment->setDate(new \DateTime());
         // check if data respect formType rules
-        $form = $this->createForm(CommentType::class, $comment, []);
+        $form = $this->createForm(CommentType::class, $comment, [
+            'method' => 'post'
+        ]);
         $form->handleRequest($request);
-
         if ($form->isSubmitted() && $form->isValid()) {
+            // If the form was submitted and is valid, update the comment object
             $comment = $form->getData();
-            // tell Doctrine you want to (eventually) save the Product (no queries yet)
+
+            // Upload the image file, if present
+            $imageFile = $form->get('image')->getData();
+            if ($imageFile) {
+                // Use the Symfony Filesystem component to move the file to public/images directory
+                $filename = $this->generateUniqueFileName().'.'.$imageFile->guessExtension();
+                $imageFile->move(
+                    $this->getParameter('/public/images/'),
+                    $filename
+                );
+                $comment->setImage($filename);
+            }
+
+            // Save the comment to the database
             $entityManager->persist($comment);
-            // actually executes the queries (i.e. the INSERT query)
             $entityManager->flush();
 
-            //return new RedirectResponse('/comments');
             return new JsonResponse(
                 ['result' => $comment->getId()]
             );
         }
-        return new JsonResponse(
-            ['result' => []]
-        );
+
+        // If the form was not submitted or is not valid, return a JSON response with the form errors
+        $errors = $this->getFormErrors($form);
+        return new JsonResponse(['errors' => $errors]);
     }
 
     #[Route('/comments', name: 'app_comments_list')]
     public function list(Request $request, EntityManagerInterface $entityManager): Response
     {
         $article = [
-            'image' => '/images/magic-photo.png',
+            'image' => '/images/corgi_in_mountain.jpg',
             'question' => 'un corgi peut-il marcher 30km dans les Vosges en une seule et même journée ?',
             'author' => 'moi',
-            'content' => "ouiuouiouiuouiuouiuouiuouiu"
+            'content' => "Ut et accumsan turpis. Aenean fermentum urna in neque convallis mollis. Sed a dictum eros. Sed ex leo, pellentesque at dictum id, gravida ac mauris. Praesent nec pellentesque odio. Duis pharetra, justo ac mattis dignissim, lorem ipsum iaculis odio, quis commodo tellus felis eget eros. Sed iaculis mi ut felis bibendum molestie. In id hendrerit ex. Phasellus gravida porta tortor tempor accumsan. Maecenas tempus enim a congue aliquam."
         ];
 
         $commentsList = $entityManager->getRepository(Comment::class)->findBy([], ['date' => 'DESC']);
-        // Opinion examples
-//        $commentsList = [
-//            0 => [
-//                'pseudo' => 'troisticha',
-//                'email' => 'troisticha@miaou.fr',
-//                'note' => '3',
-//                'text' => 'lulululululululululul',
-//                'image' => "/images/tisha.png",
-//                'date' => '15.04.2023',
-//            ],
-//            1 => [
-//                'pseudo' => 'ticha',
-//                'email' => 'ticha@miaou.fr',
-//                'note' => '5',
-//                'text' => 'miaoumiaoumioaumiaou',
-//                'image' => "/images/tisha.png",
-//                'date' => "23.01.2023"
-//            ]
-//        ];
 
-        return $this->render('comment/index.html.twig', [
+        return $this->render('comment/listAndFormPage.twig', [
             'article' => $article,
             'comments' => $commentsList,
         ]);
     }
 
+    private function getFormErrors(FormInterface $form): array
+    {
+        $errors = [];
+
+        // Loop through all form fields and get their errors
+        foreach ($form->all() as $commentFieldForm) {
+            $fieldErrors = $commentFieldForm->getErrors();
+            if (count($fieldErrors) > 0) {
+                $errors[$commentFieldForm->getName()] = (string) $fieldErrors;
+            }
+        }
+
+        return $errors;
+    }
 }
--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -8,7 +8,6 @@ use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -27,7 +26,15 @@ class CommentType extends AbstractType
             ])
             ->add('date', DateTimeType::class, [
                 'required' => false,
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd HH:mm:ss',
+                'html5' => false,
             ])
+            ->setMethod('POST')
+            ->setAction($options['action'])
+            ->setAttributes([
+                'enctype' => 'multipart/from-data'
+            ]);
         ;
     }
 
